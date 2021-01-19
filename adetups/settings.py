abf8a0979cd2de5efefc64a0eec33c133195b5a0
jafetandres <46006892+jafetandres@users.noreply.github.com>
2021-01-19 11:44:23 -0500

    cambios

--- adetups/settings.py
+++ adetups/settings.py
@@ -138,6 +138,7 @@ DATE_FORMAT = "Y-m-d"
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
